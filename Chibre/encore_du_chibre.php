d28397395aaab9bf3a5e9ebdfb868ab4b65cd5cc
paulhilaire <paul.hilaire@student.ecp.fr>
2013-03-05 15:10:06 +0100

    Add Gros chibre qui merde à la fin

--- Chibre/encore_du_chibre.php
+++ Chibre/encore_du_chibre.php
@@ -2,3 +2,6 @@
 
 echo 'Encore du chibre';
 echo 'Ceci est sur la branche Chibre';
+echo "Dans ta gueule le chibre"
+echo " Et puis aussi je sais plus parler français soggzekgnkgjs ifergbezoghzlfhztgiohzemvizmtgihamzpémofe	ziafgpohmhjmfdrgfmbjmimvhqsmvkaekghmivh:fn:VHA%IFH%BIH	MRIOVHQDSMGmmoighmoivhfiqCHMZOFHMivh	fiùPFOH	FKLcnfncm,lfjmighqlfhfmiamighalsuvhbzemighaeùrpfhùfihg'
+
