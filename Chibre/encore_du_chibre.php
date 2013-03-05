075bef83d3167b38a7e520a3c7c3efed7db5e9de
paulhilaire <paul.hilaire@student.ecp.fr>
2013-03-05 15:12:18 +0100

    Vpoila la croorection

--- Chibre/encore_du_chibre.php
+++ Chibre/encore_du_chibre.php
@@ -2,6 +2,8 @@
 
 echo 'Encore du chibre';
 echo 'Ceci est sur la branche Chibre';
-echo "Dans ta gueule le chibre"
+
+echo "Dans ta gueule le chibre";
+
 echo " Et puis aussi je sais plus parler français soggzekgnkgjs ifergbezoghzlfhztgiohzemvizmtgihamzpémofe	ziafgpohmhjmfdrgfmbjmimvhqsmvkaekghmivh:fn:VHA%IFH%BIH	MRIOVHQDSMGmmoighmoivhfiqCHMZOFHMivh	fiùPFOH	FKLcnfncm,lfjmighqlfhfmiamighalsuvhbzemighaeùrpfhùfihg';
 
